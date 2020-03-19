227a6d0b4e9d5a7d17f0b485b2acbcc4a417a8f1
Shawn Wright <volushawn@gmail.com>
2020-03-18 21:23:33 -0500

    Splash page login fixes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -20,6 +20,7 @@ module.exports = function(app) {
     res.render("signup");
   });
 
+  //load single image view
   app.get("/gallery/:id", function(req, res) {
     db.Art.findOne({ where: { id: req.params.id } }).then(function(response) {
       res.render("viewArt", {
@@ -28,14 +29,26 @@ module.exports = function(app) {
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
-      });
+  //load user page
+  app.get("/user/:id", function(req, res) {
+    // db.User.findOne({ where: { id: req.params.id } }).then(function(response) {
+    //   res.render("user", {
+    //     user: response
+    //   });
+    // });
+    res.render("user", {
+      username: req.params.id
     });
   });
+
+  // // Load example page and pass in an example by id
+  // app.get("/example/:id", function(req, res) {
+  //   db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.render("example", {
+  //       example: dbExample
+  //     });
+  //   });
+  // });
 
   // Render 404 page for any unmatched routes
   app.get("*", function(req, res) {
