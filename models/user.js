339fc8e9377f39ecab82c1ebeee5203f400e5edc
Shawn Wright <volushawn@gmail.com>
2020-03-20 15:40:37 -0500

    models push

--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 var bcrypt = require("bcryptjs");
 
-module.exports = function (sequelize, DataTypes) {
+module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
     // eslint-disable-next-line camelcase
     name: {
@@ -28,11 +28,11 @@ module.exports = function (sequelize, DataTypes) {
     }
   });
 
-  User.prototype.validPassword = function (password) {
+  User.prototype.validPassword = function(password) {
     return bcrypt.compareSync(password, this.password);
   };
 
-  User.addHook("beforeCreate", function (user) {
+  User.addHook("beforeCreate", function(user) {
     user.password = bcrypt.hashSync(
       user.password,
       bcrypt.genSaltSync(10),
