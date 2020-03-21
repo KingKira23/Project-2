2281db90d890b45b607ccd40ca3384602f230269
Andres Long <andreslong95@gmail.com>
2020-03-20 21:59:19 -0500

    Test

--- seed.js
+++ seed.js
@@ -11,26 +11,26 @@ var seed = {
   },
 
   addArt: function() {
-    db.Art.create({
-      art_name: "Being There",
-      url_link:
-        "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
-      UserId: 1
-    });
+  //   db.Art.create({
+  //     art_name: "Being There",
+  //     url_link:
+  //       "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
+  //     UserId: 1
+  //   });
 
-    db.Art.create({
-      art_name: "Being There",
-      url_link:
-        "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
-      UserId: 1
-    });
+  //   db.Art.create({
+  //     art_name: "Being There",
+  //     url_link:
+  //       "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
+  //     UserId: 1
+  //   });
 
-    db.Art.create({
-      art_name: "Being There",
-      url_link:
-        "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
-      UserId: 1
-    });
+  //   db.Art.create({
+  //     art_name: "Being There",
+  //     url_link:
+  //       "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
+  //     UserId: 1
+  //   });
   }
 };
 
