baf9ada0e9aaddac83e05d5651cf3ecf639e72a5
Denisa Dupu <dupudenisa@gmail.com>
2020-03-20 22:20:39 -0500

    merge final

--- seed.js
+++ seed.js
@@ -3,7 +3,11 @@ var db = require("./models");
 
 var seed = {
   addUser: function() {
-    db.User.create({ name: "Shawn", username: "Booboo", password: "boo" });
+    db.User.create({
+      name: "Albisareswn",
+      username: "all",
+      password: "boo"
+    }).then(this.addArt);
   },
 
   addArt: function() {
@@ -11,38 +15,23 @@ var seed = {
       art_name: "Being There",
       url_link:
         "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
-      UserId: "1"
+      UserId: 1
     });
 
     db.Art.create({
       art_name: "Being There",
       url_link:
         "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
-      UserId: "1"
+      UserId: 1
     });
 
     db.Art.create({
       art_name: "Being There",
       url_link:
         "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
-      UserId: "1"
-    });
-  },
-
-  addComment1: function() {
-    db.Comment.create({
-      comment: "This is a great art!",
-      UserId: "1",
-      ArtId: "1"
-    });
-  },
-
-  addComment2: function() {
-    db.Comment.create({
-      comment: "This is also a great art!",
-      UserId: "1",
-      ArtID: "1"
+      UserId: 1
     });
   }
 };
+
 module.exports = seed;
