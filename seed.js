25b647371abd23cdcda572e74e4e0b6dd424bc49
Andres Long <andreslong95@gmail.com>
2020-03-21 22:20:31 -0500

    seed js changes

--- seed.js
+++ seed.js
@@ -16,14 +16,6 @@ var seed = {
       url_link:
         "http://www.clker.com/cliparts/1/4/2/8/11954403871637743121zeimusu_dog_face_4.svg.hi.png",
       UserId: 1
-    }).then(this.comment);
-  },
-
-  addComment: function() {
-    db.Comment.create({
-      comment: "Very Nice",
-      ArtId: 1,
-      UserId: 1
     });
   }
 };
